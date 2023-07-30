71d51907d8d39ce4bb30a284076a320dfafcd041
Aditya tiwari <85674450+notstoicc@users.noreply.github.com>
2023-07-30 17:46:03 +0530

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,8 +1,8 @@
-function Banner({ label }) {
+function Banner({ icon, label }) {
     return (
       <div>
         <div className="border-banner flex select-none flex-row items-center justify-center space-x-1 rounded-full border px-2 py-1">
- 
+          <span className="w-5">{icon}</span>
           <span className="text-banner text-xs">{label}</span>
         </div>
       </div>
@@ -12,12 +12,13 @@ function Banner({ label }) {
   function Banners() {
     return (
       <div className="mx-auto flex flex-wrap items-center justify-center gap-1">
-        <Banner label="Anime" />
-        <Banner label="Manga" />
-        <Banner label="Movies" />
-        <Banner label="Music" />
+        <Banner icon="⚪" label="Anime" />
+        <Banner icon="⚪" label="Manga" />
+        <Banner icon="⚪" label="Movies" />
+        <Banner icon="⚪" label="Music" />
       </div>
     );
   }
 
 export default Banners;
+
