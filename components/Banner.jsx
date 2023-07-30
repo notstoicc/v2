002e20200b89dca7f61921a0a7a02228a1088628
Aditya tiwari <85674450+notstoicc@users.noreply.github.com>
2023-07-30 17:37:38 +0530

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,7 +2,7 @@ function Banner({ label }) {
     return (
       <div>
         <div className="border-banner flex select-none flex-row items-center justify-center space-x-1 rounded-full border px-2 py-1">
-          <span className="w-5">{icon}</span>
+ 
           <span className="text-banner text-xs">{label}</span>
         </div>
       </div>
